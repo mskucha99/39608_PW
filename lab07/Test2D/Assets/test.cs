226f77064e9538f91a8657e90be651d23c47ab2b
Skucha <mskucha99@wp.pl>
2020-05-15 16:14:53 +0200

    lab07 i lab08

---
+++ lab07/Test2D/Assets/test.cs
@@ -0,0 +1,19 @@
+﻿m /[]plkomkncusing System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public int x = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Test "+x);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ lab08/New Unity Project/Assets/s1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class s1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        GameObject[] kule =GameObject.FindGameObjectsWithTag("kula");
+        foreach (GameObject k in kule)
+        {
+            Destroy(k);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
