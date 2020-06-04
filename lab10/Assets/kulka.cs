4ceda281cad82d710114bdeeb67e65892fcd7920
Skucha <mskucha99@wp.pl>
2020-06-04 18:02:38 +0200

    lab10

---
+++ lab10/Assets/kulka.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kulka : MonoBehaviour
+{
+    public float speed;
+    public GameObject kuleczka;
+    public Material[] kolory;
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float y = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        transform.Translate(x, y, 0);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 pozycja = Input.mousePosition;
+            pozycja.z = 15f;
+            pozycja = Camera.main.ScreenToWorldPoint(pozycja);
+            LosujKolor();
+            Instantiate(kuleczka, pozycja, Quaternion.identity);
+        }
+    }
+    
+    void LosujKolor()
+    {
+        int rand = Random.Range(0, kolory.Length);
+        kuleczka.transform.GetComponent<Renderer>().material = kolory[rand];
+    }
+}
